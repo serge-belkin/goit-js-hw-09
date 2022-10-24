c9dd1423434d96ff6f126dc35218189d3842cba1
serge-belkin <cer_fi@yahoo.com>
2022-10-24 10:55:45 +0300

    adds task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+"use strict";
 
+const startBtn = document.querySelector('button[data-start]');
+
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+
+  stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor()
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+
+  startBtn.disabled = false;
+
+  stopBtn.disabled = true;
+});
